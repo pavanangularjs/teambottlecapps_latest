32498809d1c90dcb05b7893e8d2d2c2f922400c3
Pavan Kumar Majeti <pavanangularjs@gmail.com>
2019-03-16 14:18:39 +0530

    Vikram's UI changes with Contact Page Angular Implementation
    showing the map dynamic with store's Lat and Long values

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductStoreService } from '../../services/product-store.service';
+import { CustomerLoginSession } from '../../models/customer-login-session';
+import { CustomerSelectors } from '../../state/customer/customer.selector';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+  storeDetails: any;
+  mapURL: SafeResourceUrl;
 
-  constructor() { }
+  constructor(
+    public sanitizer: DomSanitizer,
+    private store: Store<CustomerLoginSession>,
+    private storeService: ProductStoreService) {
+
+    this.store.select(CustomerSelectors.customerLoginSessionData)
+      .subscribe(clsd => {
+        if (clsd) {
+          this.getStoreDetails();
+        }
+      });
+  }
 
   ngOnInit() {
+    this.mapURL = '';
+  }
+
+  getStoreDetails() {
+    this.storeService.getStoreDetails().subscribe(data => {
+      if (data && data.GetStoredetails) {
+        this.storeDetails = data.GetStoredetails;
+        const url = `https://maps.google.com/maps?q=${this.storeDetails.Latitude},${this.storeDetails.Longitude}&hl=es;z=14&output=embed` ;
+        this.mapURL = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+      }
+    });
   }
 
 }
